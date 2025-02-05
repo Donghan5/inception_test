f092c9964ab2ba1a84aa3f2cac4ace2bed74150d
Donghan Kim <donghank@paul-f4Ar6s8.clusters.42paris.fr>
2025-02-05 13:42:48 +0100

    test it

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -11,8 +11,8 @@ fi
 
 # echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
 # echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_HOME', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_SITEURL', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_HOME', 'https://donghank.42.fr' );" >> /var/www/wordpress/wp-config.php
+# echo "define( 'WP_SITEURL', 'https://donghank.42.fr' );" >> /var/www/wordpress/wp-config.php
 
 # echo "define( 'WP_DEBUG', true);" >> /var/www/wordpress/wp-config.php
 # echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
