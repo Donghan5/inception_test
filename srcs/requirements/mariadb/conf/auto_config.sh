e7d888a879c6362a6a67fba6ee6ecd57b9b380e0
Donghan Kim <donghank@paul-f4Br6s3.clusters.42paris.fr>
2025-02-06 12:09:57 +0100

    CPP09 ex02 remove g3 flags

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -7,13 +7,13 @@ service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 # create user with a password
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@`localhost` IDENTIFIED BY `${SQL_PASSWORD}`;"
+mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY ${SQL_PASSWORD};"
 
 # give all privileges to the user
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@`%` IDENTIFIED BY `${SQL_PASSWORD}`;"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY ${SQL_PASSWORD};"
 
 # mod sql database
-mysql -e "ALTER USER `root`@`localhost` IDENTIFIED BY `${SQL_ROOT_PASSWORD}`;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${SQL_ROOT_PASSWORD};"
 
 # reload the database
 mysql -e "FLUSH PRIVILEGES;"
