931f98949932149115948b80cb6980863aaa21e5
donghank <donghank@donghank.myguest.virtualbox.org>
2025-02-06 13:02:35 +0100

    ds

--- srcs/requirements/mariadb/conf/auto_config.sh
+++ srcs/requirements/mariadb/conf/auto_config.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # start mysql service
-exec mysqld_safe &
+service mysql start;
 
 # create a database (if it's not exist)
-mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
 # create user with a password
-mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY ${SQL_PASSWORD};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # give all privileges to the user
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY ${SQL_PASSWORD};"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # mod sql database
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${SQL_ROOT_PASSWORD};"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # reload the database
 mysql -e "FLUSH PRIVILEGES;"
 
 # shutdown
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p"{$SQL_ROOT_PASSWORD}" shutdown
 
 exec mysqld_safe
